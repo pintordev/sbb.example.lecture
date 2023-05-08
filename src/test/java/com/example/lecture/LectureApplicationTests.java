414585aab4db6430a996c48ae008a3cb441164f1
pintordev <pintor.dev@gmail.com>
2023-05-08 10:20:26 +0900

    Jump2SB lecture til 2.11

---
+++ src/test/java/com/example/lecture/LectureApplicationTests.java
@@ -0,0 +1,41 @@
+package com.example.lecture;
+
+import com.example.lecture.answer.Answer;
+import com.example.lecture.answer.AnswerRepository;
+import com.example.lecture.question.Question;
+import com.example.lecture.question.QuestionRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Autowired
+	private AnswerRepository answerRepository;
+
+	@Transactional
+	@Test
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
+
+}
