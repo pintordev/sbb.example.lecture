7bc2e3441d77e72e4390893d784934cd719d28e2
pintordev <pintor.dev@gmail.com>
2023-05-08 13:37:49 +0900

    Jump2SB lecture til 2.15

--- src/main/java/com/example/lecture/question/QuestionController.java
+++ src/main/java/com/example/lecture/question/QuestionController.java
@@ -1,11 +1,12 @@
 package com.example.lecture.question;
 
+import com.example.lecture.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,9 +25,24 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String createQuestion(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String createQuestion(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        } else {
+            this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+            return "redirect:/question/list";
+        }
     }
 }
