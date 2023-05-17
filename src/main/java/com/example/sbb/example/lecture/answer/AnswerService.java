9b09282163ded3a9cd743ec3ed4c6cc254bf5330
pintordev <pintor.dev@gmail.com>
2023-05-17 13:37:42 +0900

    Jump2SB lecture til 3.10

--- src/main/java/com/example/sbb/example/lecture/answer/AnswerService.java
+++ src/main/java/com/example/sbb/example/lecture/answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.example.sbb.example.lecture.answer;
 
+import com.example.sbb.example.lecture.DataNotFoundException;
 import com.example.sbb.example.lecture.question.Question;
 import com.example.sbb.example.lecture.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -21,5 +23,24 @@ public class AnswerService {
         answer.setAuthor(author);
         this.answerRepository.save(answer);
         return answer;
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
